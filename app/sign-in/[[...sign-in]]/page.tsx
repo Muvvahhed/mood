9473ee37512cb1b17eac76343dd8e0840686022f
Muwaheed <muwaheedmustapha@gmail.com>
2023-09-21 19:51:04 +0100

    add home link to sign in and sign up pages

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,4 +1,6 @@
 import { SignIn, currentUser } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
 const SignInPage = async () => {
@@ -7,8 +9,21 @@ const SignInPage = async () => {
     redirect('/')
   }
   return (
-    <div className="w-screen h-screen flex items-center justify-center bg-[url('/bg.svg')] bg-cover">
-      <SignIn afterSignInUrl={'/journal'} />
+    <div className="w-screen h-screen flex flex-col bg-[url('/bg.svg')] bg-cover">
+      <div className="flex p-4">
+        <Link href={'/'}>
+          <Image
+            src="/mood-high-resolution-logo-color-on-transparent-background(1).png"
+            alt="logo"
+            width={120}
+            height={120}
+            about="logo"
+          />
+        </Link>
+      </div>
+      <div className="m-auto">
+        <SignIn afterSignInUrl={'/journal'} />
+      </div>
     </div>
   )
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,4 +1,6 @@
 import { SignUp, currentUser } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
 const SignUpPage = async () => {
@@ -7,8 +9,21 @@ const SignUpPage = async () => {
     redirect('/')
   }
   return (
-    <div className="w-screen h-screen flex items-center justify-center bg-[url('/bg.svg')] bg-cover">
-      <SignUp afterSignUpUrl={'/new-user'} redirectUrl={'/new-user'} />
+    <div className="w-screen h-screen flex flex-col bg-[url('/bg.svg')] bg-cover">
+      <div className="flex p-4">
+        <Link href={'/'}>
+          <Image
+            src="/mood-high-resolution-logo-color-on-transparent-background(1).png"
+            alt="logo"
+            width={120}
+            height={120}
+            about="logo"
+          />
+        </Link>
+      </div>
+      <div className="m-auto">
+        <SignUp afterSignUpUrl={'/auth'} redirectUrl={'/auth'} />
+      </div>
     </div>
   )
 }

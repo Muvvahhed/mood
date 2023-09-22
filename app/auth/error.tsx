ab0b8859d1a72675221a2b63d76d97961e9de05a
Muwaheed <muwaheedmustapha@gmail.com>
2023-09-22 17:57:21 +0100

    fixes

---
+++ app/auth/error.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+const error = () => {
+  return (
+    <div className="h-screen w-full flex items-center justify-center bg-white/80 dark:bg-[#09090b]">
+      <div className="text-5xl text-black dark:text-white">
+        Something Went Wrong
+      </div>
+    </div>
+  )
+}
+
+export default error
